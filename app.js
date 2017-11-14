bba834c7a847796eec8e6c3f1373f79d2ac1969f
AnaisOlivares <anaisolivareshilario@gmail.com>
2017-11-14 09:55:20 -0500

    ultimando detalles de bordes

---
+++ app.js
@@ -0,0 +1,13 @@
+//document.getElementById
+//document.getElementByClassName
+//document.getElementsByTagName esto arroja el elemento
+
+var header = document.querySelector('header');
+//sintaxis del addEventListener(evento,funcion)
+window.onload.addEventListener('scroll',shrink);
+
+
+
+function shrink() {
+
+}
